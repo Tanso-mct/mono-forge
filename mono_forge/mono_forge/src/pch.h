d6aa4d2a18639bbaf4117c71535c44e7d1f7d5e7
Tanso <tansomctexp@gmail.com>
2025-08-15 15:23:04 +0900

    Created template of plugin project.

--- mono_forge/mono_forge/src/pch.h
+++ mono_forge/mono_forge/src/pch.h
@@ -6,3 +6,16 @@
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
 #include <windows.h>
+
+#include <iostream>
+#include <initializer_list>
+#include <shared_mutex>
+
+#include <string>
+#include <string_view>
+
+#include <memory>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
