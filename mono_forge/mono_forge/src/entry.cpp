189c8767253c9abdcc229774a95042cbc5148868
Tanso <tansomctexp@gmail.com>
2025-08-15 15:06:29 +0900

    The placement of each function for using riaecs is completed.

--- mono_forge/mono_forge/src/entry.cpp
+++ mono_forge/mono_forge/src/entry.cpp
@@ -1,5 +1,8 @@
 ﻿#include "mono_forge/src/pch.h"
 
+#pragma comment(lib, "riaecs.lib")
+#pragma comment(lib, "mem_alloc_fixed_block.lib")
+
 int APIENTRY wWinMain
 (
     _In_ HINSTANCE hInstance,
@@ -7,5 +10,49 @@ int APIENTRY wWinMain
     _In_ LPWSTR lpCmdLine,
     _In_ int nCmdShow
 ){
+    /*******************************************************************************************************************
+     * Create ECS World
+    /******************************************************************************************************************/
+    
+    std::unique_ptr<riaecs::IECSWorld> ecsWorld = std::make_unique<riaecs::ECSWorld>();
+    ecsWorld->SetComponentFactoryRegistry(std::move(riaecs::gComponentFactoryRegistry));
+    ecsWorld->SetComponentMaxCountRegistry(std::move(riaecs::gComponentMaxCountRegistry));
+    ecsWorld->SetPoolFactory(std::make_unique<mem_alloc_fixed_block::FixedBlockPoolFactory>());
+    ecsWorld->SetAllocatorFactory(std::make_unique<mem_alloc_fixed_block::FixedBlockAllocatorFactory>());
+    ecsWorld->CreateWorld();
+
+    /*******************************************************************************************************************
+     * Create Asset Container
+    /******************************************************************************************************************/
+
+    std::unique_ptr<riaecs::IAssetContainer> assetContainer = std::make_unique<riaecs::AssetContainer>();
+
+    /*******************************************************************************************************************
+     * Create initialize entities
+    /******************************************************************************************************************/
+
+    // TODO: Replace with actual entity creation and component addition logic
+
+    /*******************************************************************************************************************
+     * Create System loop and run it
+    /******************************************************************************************************************/
+
+    std::unique_ptr<riaecs::ISystemLoop> systemLoop = std::make_unique<riaecs::SystemLoop>();
+
+    // TODO: Replace with actual system list factory
+    systemLoop->SetSystemListFactory(std::make_unique<riaecs::EmptySystemListFactory>());
+
+    systemLoop->SetSystemLoopCommandQueueFactory(std::make_unique<riaecs::EmptySystemLoopCommandQueueFactory>());
+
+    // Execute the system loop
+    systemLoop->Initialize();
+    systemLoop->Run(*ecsWorld, *assetContainer);
+
+    /*******************************************************************************************************************
+     * Cleanup
+    /******************************************************************************************************************/
+    
+    ecsWorld->DestroyWorld();
+
     return 0;
 }
--- mono_forge/mono_forge/src/pch.h
+++ mono_forge/mono_forge/src/pch.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include "riaecs/riaecs.h"
+#include "mem_alloc_fixed_block/mem_alloc_fixed_block.h"
 
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
